beb46c8dbb14d01e3e837cb5b339be2f9489c8e9
BillKek <51540758+BillKek@users.noreply.github.com>
2024-05-09 21:30:12 +0300

    b = to build

---
+++ b.bat
@@ -0,0 +1,6 @@
+﻿
+bash wx-config-static --cppflags --static > cppflags.txt
+bash wx-config-static --libs --static > libsflags.txt
+g++ main.cpp -c main.o @cppflags.txt 
+g++ main.o -o main.exe @libsflags.txt @libsflags.txt @libsflags.txt -static
+
